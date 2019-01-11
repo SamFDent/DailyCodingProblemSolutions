c1f581f2fe7fc3bf0e89c99de8b82fa80940f40f
SamFDent <41853237+SamFDent@users.noreply.github.com>
2019-01-11 13:54:41 +0000

    Add files via upload

---
+++ Coding Problem 1.py
@@ -0,0 +1,24 @@
+'''
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+'''
+numbers = [10,15,3,7]
+x = len(numbers)
+foundnumind = 0
+
+for num in numbers:
+	if foundnumind == 1:
+		break
+	else:
+		loop = 0
+	while loop < x:
+		if foundnumind == 1:
+			break
+		elif num + numbers[loop] == 17:
+			print('Target Number Found!')
+			foundnumind = 1
+			break
+		else:
+			loop = loop + 1
+
+print('End of processing')
